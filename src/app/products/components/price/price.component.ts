d16db1e7522dc4c1ae93684ba7d41855ef5e6e00
Abel Borit <abelboritguitton@gmail.com>
2024-01-29 18:36:32 -0500

    [FEAT] lifecycle hooks

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,10 +1,132 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-price',
   templateUrl: './price.component.html',
-  styleUrls: ['./price.component.css']
+  styleUrls: ['./price.component.css'],
 })
-export class PriceComponent {
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+  @Input()
+  public price: number = 0;
 
+  /* se coloca de tipo Subscription ya que lo que me retorna la suscripción al interval es de tipo Subscription. Se hace esto con la finalidad de poder suscribirnos y desuscribirnos para evitar afectar el rendimiento de nuestra aplicación con efectos no deseados */
+  public intervalSubscription?: Subscription;
+
+  ngOnInit(): void {
+    console.log('PriceComponent: ngOnInit');
+
+    /* este interval de RxJS es un observable que empieza a emitir valores de forma secuencial basado en un periodo de tiempo */
+    this.intervalSubscription = interval(1000).subscribe((value) =>
+      console.log(`Tick: ${value}`)
+    );
+    /* si se crearán listeners con window.addEventListener() también sería bueno que se remueven en el ngOnDestroy usando el window.removeEventListener() */
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('PriceComponent: ngOnChanges');
+    console.log({ changes });
+  }
+
+  /* Se llama justo antes de que el componente o directiva va a ser destruida. Usualmente en esta parte del ciclo de vida de los componentes de Angular se usa para hacer limpiezas por ejemplo de suscripciones y no necesariamente con observables sino también puede ser con intérvalos de tiempo, algún tipo de petición que cree algún tipo de suscripción ,etc */
+  ngOnDestroy(): void {
+    console.log('PriceComponent: ngOnDestroy');
+    /* cancelar la suscripción del observable */
+    this.intervalSubscription?.unsubscribe();
+  }
 }
+
+/* ******************************************************************************************************************* */
+/* ¿Al destruirse un componente lo que persiste son las @Input y propiedades de la clase del componente? Porque si el componente se construye o destruye nuevamente entonces eso no afecta al valor del @Input price del componente price hijo ya que el valor del price reanuda a como estaba antes. Eso lleva a la pregunta de que entonces ¿El valor de un @Input persiste a pesar de que se destruye? ¿Y también sucede con las propiedades de una clase del componente? */
+/*
+No, si el componente es destruido la data que tengamos ahí no va a persistir. Ahora, estamos hablando de un único componente, en este caso PriceComponent es el que está destruyéndose y volviendo a construir, pero el componente padre, ProductComponent en ningún momento se está destruyendo, por lo que la data que tenga ProductComponent seguirá ahí, mientras que la de PriceComponent es eliminada.
+
+Pero, ¿Por qué parece que el valor se mantiene?
+Porque realmente "price" se lo estamos pasando desde ProductComponent a PriceCompoent usando [price]="currentPrice". Es decir, la variable currentPrice en todo momento se mantiene, ya que cuando se destruye PriceComponent, únicamente se destruye este y una vez vuelva a ser construido, como por el @Input le pasamos el valor de currentPrice este se mantiene actualizado.
+
+Si se destruyera el componente ProductComponent ahí sí luego volvería al valor de 10 ya que es el por defecto al crearse, pero mientras este siga vigente, el valor se mantiene.
+
+
+Por eso es importante que se identifique bien que únicamente se está destruyendo el componente hijo, el padre sigue ahí, por lo que persiste el estado, pero el valor de @Input no es persistente ni cualquier otra propiedad de clase, cuando estos componentes son destruidos y creados de nuevo volverá al valor por defecto que tuvieran (la persistencia la tenemos que agregar manualmente, como por ejemplo cuando se usa localStorage, o cuando en el onInit se hace una llamada a un endpoint para cargar la data, etc.
+*/
+
+/* ******************************************************************************************************************* */
+/* ¿Siempre debo hacer el unsubscribe() de los observables cuando se ejecute el ngOnDestroy? en que ocasiones si y en cuales no? */
+/*
+Como tal no siempre es necesario, pero es algo más complejo que dar un sí o no. Realmente en las peticiones http no es obligatorio, el unsubscribe lo maneja la propia petición y se limpia sola una vez finaliza, por lo que en la mayoría de casos, no hará falta.
+
+Ahora, el unsubscribe puede servir por ejemplo si una petición http está en curso en un modal y el usuario decide cerrar ese modal. En ese caso, puede que no quieras que termine esa petición http si el usuario sale antes de que terminara, por lo que ahí sí podrías tener un unsubscribe para prevenir esos casos.
+
+En caso de duda, se puede añadir el unsubscribe() ya que te asegurarás que se limpie, pero al final depende del contexto en el que se esté y qué se esté usando.
+
+Las peticiones http con el HttpClient se limpian solas una vez terminan, por lo que si ese siempre es el comportamiento esperado, ahí no haría falta.
+*/
+
+/* ******************************************************************************************************************* */
+/* Para poder manejar componentes con varias subscripciones, se podría crear un array de tipo Subscription e ir haciendo push para agregar la subscripción */
+/*
+  private subscriptions: Subscription[] = [];
+
+  ngOnInit(): void {
+    console.log('<< ngOnInit >>');
+    const subscription: Subscription = interval(1000).subscribe(intervalValue => {
+      const seconds = intervalValue;
+      console.log(seconds);
+    })
+
+    this.subscriptions.push(subscription);
+  }
+
+  ngOnDestroy(): void {
+    console.error('<< ngOnDestroy >>');
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+*/
+
+/* ******************************************************************************************************************* */
+/* Algunas formas de desuscribirse y darse cuenta si funciona: */
+/*
+Una cosa que se puede hacer para saber si efectivamente se destruye el observable, es agregarle un pipe con un tap y console:
+
+  segundos = 0;
+  segundos2 = 0;
+
+  subscripcion$!: Subscription
+  ngOnInit(): void {
+    console.log("pagina1 ngOnInit")
+    const numbers$ = interval(1000);
+    this.subscripcion$= numbers$.pipe(tap(()=>console.log("hola"))).subscribe(() => this.segundos++);
+  }
+  ngOnDestroy(): void {
+    // si se comenta se seguiran mostrando los logs del tap
+    this.subscripcion$.unsubscribe()
+  }
+
+  subscripciones$ = new Subscription()
+  ngOnInit(): void {
+    console.log("pagina1 ngOnInit")
+    const numbers$ = interval(1000)
+    const numbers2$ = interval(2000)
+
+    this.subscripciones$.add(numbers$.pipe(tap(()=>console.count("numbers1"))).subscribe(()=>this.segundos++))
+    this.subscripciones$.add(numbers2$.pipe(tap(()=>console.count("numbers2"))).subscribe(()=>this.segundos2++))
+
+  }
+  ngOnDestroy(): void {
+    // si se comenta, se seguira mostrando los logs del tap
+    this.subscripciones$.unsubscribe()
+  }
+
+
+En el template HTML basta con poner:
+
+  <h2>Segundos: {{ segundos }}</h2>
+  <h2>Segundos2: {{ segundos2 }}</h2>
+*/
--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,10 +1,118 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.css']
+  styleUrls: ['./product-page.component.css'],
 })
-export class ProductPageComponent {
 
+/* para los ciclos de vida de los componentes de Angular se tienen que implementar, es decir, usar la palabra reservada implements y esto le dice a Angular qué componente tiene a su disposición */
+export class ProductPageComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  public isProductVisible: boolean = false;
+  public handleVisible() {
+    this.isProductVisible = !this.isProductVisible;
+  }
+
+  public currentPrice: number = 10;
+  public handleIncreasePrice(): void {
+    this.currentPrice++;
+  }
+
+  /* Se llama antes de cualquier ciclo de vida. */
+  constructor() {
+    console.log('ProductPageComponent: constructor');
+  }
+
+  /* Justo después del constructor. El ngOnInit debería dispararse después de la carga del HTML. */
+  ngOnInit(): void {
+    console.log('ProductPageComponent: ngOnInit');
+  }
+
+  /* Antes de cualquier cambio a una propiedad pero la propiedad que detecta este ngOnChanges no es la propiedad que tenemos de isProductVisible sino la que viene basada en un input pero no estamos hablando de un input de HTML sino de un input del @Input (mandarle argumentos a un componente hijo) */
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ProductPageComponent: ngOnChanges');
+    console.log({ changes });
+  }
+
+  /* Se llama cada vez que una propiedad del componente o directiva es revisada. */
+  ngDoCheck(): void {
+    console.log('ProductPageComponent: ngDoCheck');
+  }
+
+  /* Después de ngOnInit, cuando el componente es inicializado. */
+  ngAfterContentInit(): void {
+    console.log('ProductPageComponent: ngAfterContentInit');
+  }
+
+  /* Se llama después de cada revisión del componente o directiva. */
+  ngAfterContentChecked(): void {
+    console.log('ProductPageComponent: ngAfterContentChecked');
+  }
+
+  /* Después del ngAfterContentInit. */
+  ngAfterViewInit(): void {
+    console.log('ProductPageComponent: ngAfterViewInit');
+  }
+
+  /* Llamado después de cada revisión de las vistas del componente o directiva. */
+  ngAfterViewChecked(): void {
+    console.log('ProductPageComponent: ngAfterViewChecked');
+  }
+
+  /* Se llama justo antes de que el componente o directiva va a ser destruida. */
+  ngOnDestroy(): void {
+    console.log('ProductPageComponent: ngOnDestroy');
+  }
 }
+
+/* ******************************************************************************************************************* */
+/*
+El ciclo de vida de un componente se compone de varias etapas, y aunque el ngOnInit se ejecuta después del constructor y es adecuado para inicializaciones iniciales, hay situaciones en las que las propiedades pueden no estar cargadas al momento de la renderización del componente en la vista. Esto puede generar errores si intentamos acceder a esas propiedades antes de que estén disponibles.
+
+Para evitar este problema, es recomendable realizar comprobaciones en la vista antes de utilizar esas propiedades, utilizando operadores seguros de navegación para evitar errores en caso de que las propiedades no estén definidas. Además, en casos donde la inicialización es asincrónica, como cuando se carga un valor desde un endpoint en el ngOnInit, es posible que la vista se renderice antes de que los datos estén disponibles. En estos casos, es útil utilizar directivas como *ngIf para condicionar la renderización de partes de la vista hasta que los datos estén listos, asegurando así que no haya errores debido a datos faltantes.
+*/
+
+/* Aquí tenemos dos razones por las que usamos el implements:
+  - La primera, para tener el strong type checking que nos ofrece TypeScript, por lo que es una buena práctica añadirlo.
+  - Y la segunda, a la hora de hacer el build de producción. Ten en cuenta que en desarrollo cargamos todos los módulos, pero al hacer el build angular intenta ser eficiente. Ten en cuenta eso ya que puede que en desarrollo algo nos funcione, pero luego en producción no. También para alguna herramienta de angular, hacer estadísticas, etc podría servir esta información.
+
+Actualmente se podría decir que por defecto es indiferente, y podríamos agregar el ngOnInit sin el implements, pero es una buena práctica.
+*/
+
+/* ******************************************************************************************************************* */
+/*
+Estas implementaciones de:
+  - ngAfterContentInit
+  - ngAfterContentChecked
+  - ngAfterViewInit
+  - ngAfterViewChecked
+
+Se usan muy pocas veces aunque sí se usan y algunos ejemplos podría ser cuando se utiliza algún paquete de terceros o plugin o algún sistema de gráficas como para recalcular las dimensiones que se tienen en pantalla.
+*/
+
+/* ******************************************************************************************************************* */
+/* Limpieza en ngDestroy se comenta usar este método para hacer limpieza en caso de haber creado en el ngOnInit algún tipo de listener, observable, timer, etc.. La duda es ¿Solo se debe hacer esa limpieza si se crean en el ngOnInit o también si se implementan en algún otro método por ejemplo, al pulsar un botón llama un método que implementa un listener, observable o timer? */
+/*
+El uso del ngOnDestroy para limpiar recursos es especialmente importante cuando estos recursos se crean dentro del ciclo de vida del componente, como en ngOnInit. Sin embargo, también es recomendable utilizar ngOnDestroy para limpiar recursos que hayan sido creados en otros métodos del componente, como cuando un botón que llama a un método que implementa un listener, observable o timer. Esto te asegurará una gestión adecuada de los recursos y evitar fugas de memoria en la aplicación.
+*/
