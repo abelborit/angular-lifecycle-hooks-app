148df53e2d6e03ef005c948be27f4cd4d81a17fa
Abel Borit <abelboritguitton@gmail.com>
2024-01-29 15:55:31 -0500

    [FEAT] add routes & update HTML

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'product', component: ProductPageComponent },
+      { path: '**', redirectTo: 'product' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ProductsRoutingModule { }
+export class ProductsRoutingModule {}
